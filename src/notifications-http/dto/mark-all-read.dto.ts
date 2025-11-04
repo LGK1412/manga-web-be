2254532c7000df1532a71217b1e423285546cc0f
huynguyenk18fpt <nguyenhhce181330@fpt.edu.vn>
2025-11-05 00:11:45 +0700

    có vẻ xong sprint 3.1

--- src/notifications-http/dto/mark-all-read.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsMongoId, IsOptional, IsString } from "class-validator";
-
-export class MarkAllReadDto {
-  @IsOptional()
-  @IsString()
-  @IsMongoId()
-  user_id?: string; // nếu không truyền sẽ lấy từ JWT
-}
